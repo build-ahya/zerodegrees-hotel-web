3c3ff4bac0b16eb11b04c0972dfc44832ef3518d
wigitnigeria <service.ahya@gmail.com>
2025-11-08 12:16:57 +0100

    fix search page error

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,16 @@
+import { Suspense } from 'react';
 import { Viewport } from 'next';
+import Loading from '@/components/loading';
+// import { SearchSection1, SearchSection2 } from '@/components/sections';
+import dynamic from 'next/dynamic';
 import constants from '@/constants';
-import { SearchSection1 } from '@/components/sections';
+
+const SearchSection1 = dynamic(
+  () => import('@/components/sections/search/Section1'),
+  {
+    loading: () => <Loading />,
+  }
+);
 
 export const viewport: Viewport = {
   themeColor: '#0C3150',
@@ -9,5 +19,9 @@ export const metadata = {
   title: `Search Room | ${constants.brandName}`,
 };
 export default function Search() {
-  return <SearchSection1 />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <SearchSection1 />
+    </Suspense>
+  );
 }
